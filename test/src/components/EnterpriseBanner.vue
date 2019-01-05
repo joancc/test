20f0a56ee5ea420264a49232ecdadb1e0e675de7
Yocelin Garcia Romero <garcia_romero.y@hotmail.com>
2019-01-04 18:37:41 -0600

    The branches component is ready to reuse others components from companies. Is missing find the way to personalize the components depending on the api data

--- test/src/components/EnterpriseBanner.vue
+++ test/src/components/EnterpriseBanner.vue
@@ -1,14 +1,25 @@
 <template>
   <div class="select-title">
-    <div class="icon companies"></div>
-    <h4>{{selectBranchTitle}}</h4>
+    <div
+      class="icon"
+      :class="[company ? 'companies' : '', warehouse ? 'warehouse' : '', office ? 'offices' : '', store ? 'stores' : '',]"
+    ></div>
+    <h4 v-if="company">{{selectBranchTitle}}</h4>
+    <h4 v-if="warehouse">Almacenes</h4>
+    <h4 v-if="office">Oficinas</h4>
+    <h4 v-if="store">Tiendas</h4>
   </div>
 </template>
 <script>
 export default {
   data: function() {
     return {
-      selectBranchTitle: "Empresas"
+      selectBranchTitle: "Empresas",
+      enterpriseIcon: "icon warehouse",
+      company: true,
+      warehouse: false,
+      office: false,
+      store: false
     };
   }
 };
--- test/src/components/ItemCompany.vue
+++ test/src/components/ItemCompany.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="item company">
+    <!--Is needed to change the company class for warehouse class if needed-->
     <button class="select-item" :disabled="!isactive ? '' : disabled">
       <!-- The button has or not a disabled property-->
       <div class="info">
         <p>
           <strong>{{warehouseTitle}}</strong>
+          <!--Find the way of use isactive to active the display none property-->
+          <!--<span v-bind:style="{ display: none }"></span>-->
           <span v-bind:class="{'on': isactive}"></span>
           <i v-bind:class="{'fg-lock-alt': !isactive}"></i>
           <span v-if="keyType == 'RFC'">{{keyType}}: {{rfc}}</span>
